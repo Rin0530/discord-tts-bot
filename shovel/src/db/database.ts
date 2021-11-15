d2fe83907b1b197ff247e09e629b3818241d7b50
BuildTools <unconfigured@null.spigotmc.org>
2021-11-15 10:19:04 +0900

    データベースのユーザー名に環境変数を使うように変更

--- shovel/src/db/database.ts
+++ shovel/src/db/database.ts
@@ -3,7 +3,7 @@ import * as mariadb from "mariadb";
 
 const pool = mariadb.createPool({
     host: "mariadb",
-    user: "rin",
+    user: process.env["MARIADB_USER"],
     password: process.env["MARIADB_PASSWORD"],
     database: process.env["MARIADB_DATABASE"]
 });
