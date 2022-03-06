3b9a7e21e7596d63b93dd988bbcad9e37a8b84c1
Rin0530 <onochanbasket@gmail.com>
2022-03-06 07:59:50 +0000

    存在しない外部キーを参照する構成になっていたので順番を修正

--- mysql/init.d/create_tables.sql
+++ mysql/init.d/create_tables.sql
@@ -7,6 +7,12 @@
    `pitch` INT NOT NULL ,
    PRIMARY KEY (`id`));
 
+ -- ギルド管理テーブル
+ CREATE TABLE IF NOT EXISTS `tts`.`guilds` (
+   `id` CHAR(18) NOT NULL ,
+   `name` NVARCHAR(255) NOT NULL ,
+   PRIMARY KEY (`id`));
+
  -- 単語辞書管理テーブル
  CREATE TABLE IF NOT EXISTS `tts`.`wordsDict` (
    `before` VARCHAR(255) PRIMARY KEY,
@@ -15,9 +21,3 @@
    FOREIGN KEY (`guild_id`) 
     REFERENCES `guilds`(`id`)
  );
-
- -- ギルド管理テーブル
- CREATE TABLE IF NOT EXISTS `tts`.`guilds` (
-   `id` CHAR(18) NOT NULL ,
-   `name` NVARCHAR(255) NOT NULL ,
-   PRIMARY KEY (`id`));
