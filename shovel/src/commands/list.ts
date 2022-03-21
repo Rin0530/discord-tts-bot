9c95f3b5846b96e461990d0b10a163e8cb5ddfb1
Rin0530 <onochanbasket@gmail.com>
2022-03-21 23:16:40 +0900

    単語辞書の送信方法をコードブロックとファイルから選択するように変更

--- shovel/src/commands/list.ts
+++ shovel/src/commands/list.ts
@@ -1,27 +1,63 @@
 import { ApplicationCommandData, CommandInteraction } from "discord.js/typings/index.js"
-import { createWriteStream, exists } from "fs"
+import { createWriteStream } from "fs"
 import { getWords } from "../db/database";
+
+const METHOD = {
+    file: "file",
+    codeBlock: "codeBlock"
+}
 
 export const registerList:ApplicationCommandData = {
     name: "list",
-    description: "Send word dictionary file."
+    description: "Send word dictionary.",
+    options: [
+        {
+            name: "method",
+            description: "TransmissionMethod",
+            type: "STRING",
+            required: true,
+            choices: [
+                {
+                    name: METHOD.codeBlock,
+                    value: METHOD.codeBlock
+                },
+                {
+                    name: METHOD.file,
+                    value: METHOD.file
+                }
+            ]
+        }
+    ]
 }
 
 export async function list(interaction:CommandInteraction) {
     const guildId = interaction.guildId;
+    const method = interaction.options.getString("method", true)
     if(!guildId)
         return interaction.reply("this command is unable exclude textChannel!");
     
     await interaction.reply("処理中");
 
-    const result = await getWords(guildId);
+    const result = await getWords(guildId);        
     if(result["before"] === "")
         return interaction.editReply("辞書ファイルは存在しません")
     
-    const stream = createWriteStream("./list.txt")
-    for(let before in result)
-        stream.write(`${before}, ${result[before]}\n`)
-    stream.end("\n")
-
-    interaction.editReply({files: ["./list.txt"]})
+    switch(method){
+        case METHOD.codeBlock:
+            let text = "\`\`\`\n"
+            for(let before in result)
+                text += `${before}, ${result[before]}\n`
+            text += "\`\`\`"
+            interaction.editReply(text)
+            break;
+        case METHOD.file:
+            const stream = createWriteStream("./list.txt")
+            for(let before in result)
+                stream.write(`${before}, ${result[before]}\n`)
+            stream.end("\n")
+            interaction.editReply({files: ["./list.txt"]})
+            break;
+        default:
+            interaction.editReply("送信方法を指定してください")
+    }    
 }
