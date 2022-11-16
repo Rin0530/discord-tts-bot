24c5b8cef706dd272608cc2b43847adcd6de31fc
Rin0530 <onochanbasket@gmail.com>
2022-11-16 21:45:37 +0900

    CHAR(20)をunsigned biugintに変更

--- mysql/init.d/create_tables.sql
+++ mysql/init.d/create_tables.sql
@@ -3,13 +3,13 @@
 
  -- ピッチ管理テーブル
  CREATE TABLE IF NOT EXISTS `tts`.`pitch` (
-   `id` CHAR(18) NOT NULL ,
+   `id` bigint(18) unsigned NOT NULL ,
    `pitch` INT NOT NULL ,
    PRIMARY KEY (`id`));
 
  -- ギルド管理テーブル
  CREATE TABLE IF NOT EXISTS `tts`.`guilds` (
-   `id` CHAR(18) NOT NULL ,
+   `id` bigint(18) unsigned NOT NULL ,
    `name` NVARCHAR(255) NOT NULL ,
    PRIMARY KEY (`id`));
 
@@ -17,7 +17,7 @@
  CREATE TABLE IF NOT EXISTS `tts`.`wordsDict` (
    `before` VARCHAR(255) NOT NULL,
    `after` VARCHAR(255) NOT NULL,
-   `guild_id` CHAR(18) NOT NULL ,
+   `guild_id` bigint(18) unsigned NOT NULL ,
    PRIMARY KEY (`before`,`guild_id`),
    FOREIGN KEY (`guild_id`) 
     REFERENCES `guilds`(`id`)
