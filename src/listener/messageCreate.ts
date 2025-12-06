696e0e3c354a24c37c91148e26bf77f5cf9f2df1
Rin0530 <51035562+Rin0530@users.noreply.github.com>
2025-12-07 00:30:10 +0900

    channge to interactionMetadata (#37)

--- src/listener/messageCreate.ts
+++ src/listener/messageCreate.ts
@@ -2,7 +2,7 @@ import { Message } from "discord.js";
 import { addQue } from "../voice/wordsQue";
 
 export async function onMessageCreate(message: Message) {
-    if (message.interaction)
+    if (message.interactionMetadata)
         return;
     await addQue(message)
 }
